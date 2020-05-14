68817dd8d2366ece59df7dbde5dc8148711f9137
smaxa20 <scottkmaxa@gmail.com>
2020-05-14 01:52:20 -0700

    Print a little prettier.

--- main.py
+++ main.py
@@ -18,29 +18,41 @@ data = data.fillna(0)
 result = "Complete"
 
 #filter() - translatR
+print("Filter **********\n")
 print(r.filter(data, "'GN/LS' >= 10", "'RESULT' ==", result, "'YARD LN' < 0"))
+print()
 #filter() - pandas
 print(data.loc[(data["GN/LS"] >= 10) & (data["RESULT"] == result) & (data["YARD LN"] < 0)])
 
 #arrange() - translatR
+print("\n\nArrange **********\n")
 print(r.arrange(data, "desc(GN/LS)", "DN"))
+print()
 #arrange() - pandas
 print(data.sort_values(["GN/LS", "DN"], ascending=[True, False]))
 
 #select() - translatR
+print("\n\nSelect **********\n")
 print(r.select(data, "ODK", "HASH"))
+print()
 #select() - pandas
 print(data[['ODK','HASH']])
 
 #rename() - translatR
+print("\n\nRename **********\n")
 print(r.rename(data, {'ODK':'odk'}, {'HASH':'hash'})) #okay that it isnt the exact syntax?
+print()
 print(r.rename(data, {'odk':'ODK'}, {'hash':'HASH'}))
+print()
 #rename() - pandas [rename and then set name back]
 print(data.rename(columns={"ODK":"odk"}))
+print()
 print(data.rename(columns={"odk":"ODK"}))
 
 #mutate()
+print("\n\nMutate **********\n")
 print(r.mutate(data, "test1=LN-DIST"))
+print()
 #print(data.mutate(test = dn - ln, test1 = dn * 25))
 print(data.assign(test = lambda x: x.DN - x.DIST))
 #transmute()
